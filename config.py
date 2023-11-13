9a4e5dce765d50a87570542183693f8e54432192
sovdee <10354869+sovdeeth@users.noreply.github.com>
2023-11-12 18:48:20 -0800

    Update config.py

--- config.py
+++ config.py
@@ -14,10 +14,16 @@ AO3_USERNAME = ""
 AO3_PASSWORD = ""
 
 # Server IDs where the ability for anyone to delete bot messages is disabled
-servers_no_deletion = set([123456789012345678, 234567890123456789])
+servers_no_deletion = {123456789012345678, 234567890123456789}
 
 # Server IDs where reacting to a series message to get fic info is disabled
-servers_no_reacts = set([123456789012345678, 234567890123456789])
+servers_no_reacts = {123456789012345678, 234567890123456789}
 
-# User IDs of bots that should be ignored by the bot
-bots_allow = set([123456789012345678])
+# User IDs of bots that should not be ignored by the bot
+bots_allow = {123456789012345678}
+
+# The maximum number of links to respond to at once
+max_links = 3
+
+# The prefix users should use to prevent the bot from responding to a link
+prefix = "!"
